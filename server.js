8860b050e60086cc9829712bc7b2b22388e8f2b1
Tarek <tarekkechich@gmail.com>
2025-11-02 23:36:50 +0100

    initial commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); // autorise tout domaine
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+  next(); 
 });
 
 // Fonction récursive qui copie `r` dans `n` dans chaque sous-objet
